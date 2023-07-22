f5ff9978814386253200ab7d3386a566b93a4c6f
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-22 05:42:18 +0100

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -29,7 +29,7 @@ navigate('/')
   <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl' onClick={handleSignOut} >Sign Out</button>
     </div>
     </div>
-    <div className='flex justify-between items-center my-12 py-8 rounded-div'>
+  <div className='flex justify-between items-center my-12 py-8 rounded-div'>
       <div className='w-full min-h-[300px]'>
         <h1 className='text-2xl font-bold py-4'>Watch List</h1>
         <SavedCoin/>
