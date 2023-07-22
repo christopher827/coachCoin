8a81c51e9a2f046809c456cd28d12b429d9eae2d
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-22 05:32:56 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -18,17 +18,6 @@ export const db=getFirestore(app)
 
 export default app
 
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyCx2iKs4eVArGxvW0NgItB8_tdf7RHrtk4",
-  authDomain: "coachcoin-4324c.firebaseapp.com",
-  projectId: "coachcoin-4324c",
-  storageBucket: "coachcoin-4324c.appspot.com",
-  messagingSenderId: "465744926408",
-  appId: "1:465744926408:web:3a0eccf6ec3de48084b7fd",
-  measurementId: "G-4XQ37KZ206"
-};
- */
 
 //chatGPT
 /*service cloud.firestore {
