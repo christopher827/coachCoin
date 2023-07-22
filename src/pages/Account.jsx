bee5cc222a6fe53a5069d78008da23fda41bdd0a
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-22 05:48:38 +0100

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -14,7 +14,7 @@ navigate('/')
 } catch (e) {
 console.log(e.message)
 }
-  }
+}
   if (user) {
   return(   <div className='max-w-[1140px] mx-auto'>
   <div className='flex justify-between items-center my-12 py-8 rounded-div'>
