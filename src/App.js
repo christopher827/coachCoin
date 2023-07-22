43da2cc16ac3b021b4b08900ac408235d04eff99
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-22 05:31:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
 const [coins,setCoins]=useState([])
-//const url='https://api.coingecko.com/api/v3/coins/markets/?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
 const url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&locale=en'
 
 useEffect(()=>{
