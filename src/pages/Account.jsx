2440e8db26d57658fc5e095f6c40f35086db0dfa
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-22 05:38:33 +0100

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -25,7 +25,7 @@ try {
     </div>
     </div>
     
-    <div>
+  <div>
       <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl' onClick={handleSignOut} >Sign Out</button>
     </div>
     </div>
